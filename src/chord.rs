94169a2dd33d4874fcb6fd74a307827caa6239e5
t-bltg <tf.bltg@gmail.com>
2026-02-11 12:05:13 +0100

    add sharp variants

--- src/chord.rs
+++ src/chord.rs
@@ -1,6 +1,19 @@
 use itertools::join;
 
 #[derive(Debug, Clone, Copy)]
+pub struct Capo {
+    pub fret: u8,
+}
+
+impl Capo {
+    pub const fn new(fret: u8) -> Self {
+        Self {
+            fret: fret,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Barre {
     // Guitar normally call the string with the highest pitch, or the thinnest,
     // the 1st string; and the thickest, lowest-pitched string, the 6th string.
@@ -8,11 +21,11 @@ pub struct Barre {
     pub from_string: u8,
     pub to_string: u8,
 
-    pub fret: u16,
+    pub fret: u8,
 }
 
 impl Barre {
-    pub const fn new(fret: u16) -> Self {
+    pub const fn new(fret: u8) -> Self {
         Self {
             from_string: 0,
             to_string: 5,
@@ -25,7 +38,20 @@ pub const BARRE_FRET1: Barre = Barre::new(1);
 pub const BARRE_FRET2: Barre = Barre::new(2);
 pub const BARRE_FRET3: Barre = Barre::new(3);
 
-pub const FRETBOARD: &str = "◯ ◯ ◯ ◯ ◯ ◯
+pub const CAPO_FRET1: Capo = Capo::new(1);
+pub const CAPO_FRET2: Capo = Capo::new(2);
+pub const CAPO_FRET3: Capo = Capo::new(3);
+pub const CAPO_FRET4: Capo = Capo::new(4);
+pub const CAPO_FRET5: Capo = Capo::new(5);
+pub const CAPO_FRET6: Capo = Capo::new(6);
+// pub const CAPO_FRET7: Capo = Capo::new(7);
+// pub const CAPO_FRET8: Capo = Capo::new(8);
+// pub const CAPO_FRET9: Capo = Capo::new(9);
+
+pub const FRETS: [char; 10] = ['🯰', '🯱', '🯲', '🯳', '🯴', '🯵', '🯶', '🯷', '🯸', '🯹'];
+
+pub const FRETBOARD: &str = "\
+◯ ◯ ◯ ◯ ◯ ◯
 ╒═╤═╤═╤═╤═╕
 │ │ │ │ │ │
 ├─┼─┼─┼─┼─┤
@@ -45,6 +71,7 @@ pub struct Chord<'a> {
     // '1' indicates that we place a finger on the first fret, '2' on the 2nd fret, and etc.
     pub pattern: &'a str,
     pub names: &'a [&'a str],
+    pub capo: Option<Capo>,
     pub barre: Option<Barre>,
 }
 
@@ -53,12 +80,14 @@ impl<'a> Chord<'a> {
         short_names: &'a [&'a str],
         pattern: &'a str,
         names: &'a [&'a str],
+        capo: Option<Capo>,
         barre: Option<Barre>,
     ) -> Self {
         Self {
             short_names: short_names,
             pattern: pattern,
             names: names,
+            capo: capo,
             barre: barre,
         }
     }
@@ -74,6 +103,12 @@ impl<'a> Chord<'a> {
     pub fn fretboard(&self) -> String {
         let mut board: Vec<char> = FRETBOARD.chars().collect();
 
+        if let Some(capo) = &self.capo {
+            for i in 0..5 {
+                board[12 + 2 * i + 1] = FRETS[capo.fret as usize]
+            }
+        }
+
         if let Some(barre) = &self.barre {
             for i in barre.from_string..barre.to_string * 2 + 1 {
                 board[i as usize + 24 * barre.fret as usize] = '-'
@@ -83,7 +118,7 @@ impl<'a> Chord<'a> {
         for (i, ch) in self.pattern.chars().enumerate() {
             let idx: usize = i * 2;
             if ch == 'x' {
-                board[idx] = ch
+                board[idx] = '✕'
             } else {
                 let value: usize = ch.to_digit(10).unwrap() as usize;
                 board[idx] = ' ';
